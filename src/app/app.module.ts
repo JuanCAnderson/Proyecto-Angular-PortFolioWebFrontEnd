4a2f39078f8b3d86d785c2a83d293a4b064c7840
JuanCAnderson <andersonjuancruz97@gmail.com>
2022-05-29 18:11:05 -0300

    Actualizacion Portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { PrincipalComponent } from './componentes/principal/principal.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import {PortfolioService} from './servicios/portfolio.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    PrincipalComponent,
+    SobreMiComponent,
+    HabilidadesComponent,
+    EducacionComponent,
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
